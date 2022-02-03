60a8fccddb0683d20676ec6d288d8bd888cb631e
kobataka8455 <kobataka0505@gmail.com>
2022-02-03 10:52:12 +0900

    add parameters

---
+++ src/function/parameters.ts
@@ -0,0 +1,28 @@
+// オプションパラメータを持つ関数
+export const isUserSignedIn = (userId: string, username?: string): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 1: User is signed in! Username is', username);
+    return true;
+  } else {
+    console.log('Function parameters sample 2: User is not signed in!');
+    return true;
+  }
+};
+
+// Defaultパラメータを持つ関数
+export const isUserSignedIn2 = (userId: string, username = 'aaaa'): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 3: User is signed in! Username is', username);
+    return true;
+  } else {
+    console.log('Function parameters sample 4: User is not signed in!');
+    return true;
+  }
+};
+
+// レストパラメーターを持つ関数の例
+export const sumProductsPrice = (...productsPrice: number[]): number => {
+  return productsPrice.reduce((prevTotal: number, productPrice: number) => {
+    return prevTotal + productPrice;
+  }, 0);
+};
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,21 @@
 // world.sayHello(root);
 
 // 基本の型定義
-import { primitiveSample, notExitstSample, anySample, unknownSample } from './basic';
-unknownSample();
-anySample();
-notExitstSample();
-primitiveSample();
+// import { primitiveSample, notExitstSample, anySample, unknownSample } from './basic';
+// unknownSample();
+// anySample();
+// notExitstSample();
+// primitiveSample();
+
+// 関数の型定義
+import { logMessage } from './function/basic';
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters';
+logMessage('hogehoge');
+
+isUserSignedIn('ABC', 'piyopiyo');
+isUserSignedIn('hogehoge');
+
+isUserSignedIn2('ABC');
+isUserSignedIn2('hogehoge');
+const sum = sumProductsPrice(1000, 200, 300, 400, 600);
+console.log('Function parameters sample 5: Total price:', sum);
